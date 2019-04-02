03c9f94a4196b7d4c66e093b3d7753e06a32a5f2
leechang0423 <leechang0423@naver.com>
2019-04-02 12:49:04 +0900

    first commit

---
+++ src/main/java/me/anpan/usermanage/UsermanageApplication.java
@@ -0,0 +1,13 @@
+package me.anpan.usermanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsermanageApplication.class, args);
+    }
+
+}
